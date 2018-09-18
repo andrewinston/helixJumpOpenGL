dffe66f35bfab8852bdce8328158708e9c12d8b2
Usuario <a.winston.s97@gmail.com>
2018-09-18 08:06:14 -0300

    Erasing useless comments

--- main.cpp
+++ main.cpp
@@ -5,9 +5,6 @@
 #include <string.h>
 #include <vector>
 #include <bits/stdc++.h>
-float tAtual = 2.0; //um ponto é renderizado na reta
-int delta = 1; //= 1 ou -1... variação positiva ou negativa de tAtual, quando animacao = 1
-int showGuideLines = true;
 double ycam=0;
 double xcam=0;
 double zcam=0;
@@ -18,29 +15,23 @@ double rot = 0;
 //x: pra dentro da tela
 //z: pra direita
 
-//vetor que contem as abscissas dos pontos
-vector<float> vx;
-//vetor que contem as ordenadas dos pontos
-vector<float> vy;
-
-enum {ADICIONARREMOVER, ALTERAR};
-int estado = ADICIONARREMOVER;
-int pontoAtual = 0;
-
-pair<double, double> obstaculos[50];
 
 double escala = .2;
-const double ACEL_COLISAO = .18*escala;
+const double ACEL_COLISAO = .16*escala;
 const double ACEL_GRAVIDADE = 0.01*escala;
-const double RAIO_ESFERA = 0.5*escala;
+const double RAIO_ESFERA = 0.2*escala;
 const int SLICES = 100; //quantidade de slices que dividirão a esfera
 const double RAIO_CILINDRO = 1*escala;
-const double DISTANCIA_BOLA_CILINDRO = 3*escala;
+const double DISTANCIA_BOLA_CILINDRO = 4*escala;
 const double RAIO_OBSTACULO = 2.5*escala;//RAIO_CILINDRO+DISTANCIA_BOLA_CILINDRO;
 const double PI = 3.14159265359;
 const double ALTURA_OBSTACULO = 1*escala;
 const double DISTANCIA_ENTRE_OBSTACULOS = 10*escala;
 const double VELOCIDADE_MAXIMA = 1*escala;
+const int QUANTIDADE_DE_OBSTACULOS = 200;
+
+
+pair<double, double> obstaculos[QUANTIDADE_DE_OBSTACULOS];
 class Bola{
 private:
 	double x, y, z;
@@ -75,16 +66,10 @@ public:
 		if(vy < -VELOCIDADE_MAXIMA) vy = -VELOCIDADE_MAXIMA;
 	}
 	void desenha(){
-		/*glColor3f(1,1,1);
-		glPushMatrix();
-			//glTranslatef(x, y, z);
-			glutSolidSphere(RAIO_ESFERA, SLICES, SLICES);
-		glPopMatrix();*/
 		upd(DISTANCIA_BOLA_CILINDRO*cos(rot), y, DISTANCIA_BOLA_CILINDRO*sin(rot));
 		glColor3f(1, 0, 0);
 		glPushMatrix ();
 			glTranslatef(x, y, z);
-			//glRotatef       (zRotated, 0,0,1);
 			glutSolidSphere (RAIO_ESFERA, SLICES, SLICES);
 		glPopMatrix ();
 	}
@@ -132,15 +117,7 @@ void setorCilindrico(double x, double y, double z, double h, double raio, double
 
 		glVertex3f(x-cos(angle)*raio, y, z-sin(angle)*raio);
 		glVertex3f(x-cos(angle)*raio, y-h, z-sin(angle)*raio);			
-		/*glBegin(GL_TRIANGLES);
-			glVertex3f(x+sin(angle)*raio, y, z+cos(angle)*raio);
-			glVertex3f(x+sin(angle+inc)*raio, y-h, z+cos(angle+inc)*raio);
-			glVertex3f(x+sin(angle+2*inc)*raio, y, z+cos(angle+inc)*raio);
-			glVertex3f(x+sin(angle)*raio, y-h, z+cos(angle)*raio);
-			glVertex3f(x+sin(angle+inc)*raio, y, z+cos(angle+inc)*raio);
-			glVertex3f(x+sin(angle+2*inc)*raio, y-h, z+cos(angle+2*inc)*raio);
-			
-		glEnd();*/
+
 	}
 	glEnd();
 	//glColor3f(0, 0, 0);
@@ -148,19 +125,10 @@ void setorCilindrico(double x, double y, double z, double h, double raio, double
 	for(double angle = anguloFim; angle < 2*PI-inc; angle += inc){
 		glVertex3f(x-cos(angle)*raio, y, z-sin(angle)*raio);
 		glVertex3f(x-cos(angle)*raio, y-h, z-sin(angle)*raio);
-		
-		/*glBegin(GL_TRIANGLES);
-			glVertex3f(x+sin(angle)*raio, y, z+cos(angle)*raio);
-			glVertex3f(x+sin(angle+inc)*raio, y-h, z+cos(angle+inc)*raio);
-			glVertex3f(x+sin(angle+2*inc)*raio, y, z+cos(angle+inc)*raio);
-			glVertex3f(x+sin(angle)*raio, y-h, z+cos(angle)*raio);
-			glVertex3f(x+sin(angle+inc)*raio, y, z+cos(angle+inc)*raio);
-			glVertex3f(x+sin(angle+2*inc)*raio, y-h, z+cos(angle+2*inc)*raio);
-		glEnd();*/
+	
 	}
 	glEnd();
-	
-	//glColor3f(0, 0, 0);
+
 	glBegin(GL_TRIANGLES);
 		glVertex3f(x, y, z);
 		glVertex3f(x, y-h, z);
@@ -170,7 +138,6 @@ void setorCilindrico(double x, double y, double z, double h, double raio, double
 		glVertex3f(x-cos(anguloIni)*raio, y-h, z-sin(anguloIni)*raio);
 	glEnd();
 	
-	//glColor3f(1, 1, 1);
 	glBegin(GL_TRIANGLES);
 		glVertex3f(x, y, z);
 		glVertex3f(x, y-h, z);
@@ -206,11 +173,6 @@ void cilindro(double x, double y, double z, double h, double raio){
 void desenhaCilindro(){
 	glColor3f(0, 1, 0);
 	cilindro(0, 0, 0, 200, 1);
-	/*glPushMatrix();
-		glTranslatef(0, 0, 0);
-		glRotatef(90, 1, 0, 0);
-		gluCylinder(gluNewQuadric(), 1, 1, 200, SLICES, SLICES);
-	glPopMatrix();*/
 }
 
 void desenhaObstaculos(){
@@ -224,14 +186,8 @@ bool colisao(){
 	bool colidiu = 0;
 	double rotgraus = (180.0/PI)*rot;
 	for(int i = 0; i < 50; i++){
-		/*if(bola.getY() <= -DISTANCIA_ENTRE_OBSTACULOS*i-1+.1*escala && bola.getY() >= -DISTANCIA_ENTRE_OBSTACULOS*i-1){
-			return 1;
-		}*/
 		if(bola.getY()+RAIO_ESFERA < -DISTANCIA_ENTRE_OBSTACULOS*i-1-ALTURA_OBSTACULO) continue;
 		if(bola.getY()+RAIO_ESFERA <= -DISTANCIA_ENTRE_OBSTACULOS*i-1 && (rotgraus <= obstaculos[i].first || rotgraus >= obstaculos[i].second)) {
-			/*cout << "COLIDIU EM " << i << endl;
-			cout << "setor: " << "("<<obstaculos[i].first<<", "<<obstaculos[i].second<<")\n";
-			cout << "rot: " << rotgraus << endl;*/
 			return 1;
 		}
 	}
@@ -239,18 +195,12 @@ bool colisao(){
 }
 
 void funcaoDisplay() {
-	//bola.upd(xbola+DISTANCIA_BOLA_CILINDRO*cos(rot), ybola, zbola+DISTANCIA_BOLA_CILINDRO*sin(rot));
-	//bola.setRot(rot);
 	bola.move();
 	if(colisao()){
 		bola.colide();
 	}
-	//glFrustum(-300, 300, -300, 300, 0, 2000);
-	//gluPerspective(60, 1, 0, 300);
-	//glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glOrtho(-100, 100, -100, 100, 0, 100);
-//	gluOrtho2D(-8, 8, -8, 8);
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
@@ -258,10 +208,7 @@ void funcaoDisplay() {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f (0.8, 0.2, 0.1);              // Red ball displaced to left.
 	gluLookAt(bola.getX()*(RAIO_ESFERA+.4), bola.getY()+.1, bola.getZ()*(RAIO_ESFERA+.4), bola.getX(),bola.getY(),bola.getZ(), 0, 1, 0);
-	//gluLookAt(bola.getX(),bola.getY()+.1,bola.getZ(), bola.getX(),bola.getY(), bola.getZ(), 0, 1, 0);
-	//printf("(%f, %f, %f)\n", bola.getX(), bola.getY(), bola.getZ());
 	desenhaObstaculos();
-	//desenhaCilindro();
 	bola.desenha();
 	glutSwapBuffers();
 	glFlush();
@@ -271,18 +218,6 @@ void funcaoKeyboard(unsigned char key, int x, int y) {
 	if(key == 'z') {
 		exit(-1);
 	}
-	/*if(key == 'a') xbola-=0.1;
-	if(key == 'q') xbola+=0.1;
-	if(key == 's') ybola-=0.1;
-	if(key == 'w') ybola+=0.1;
-	if(key == 'd') zbola-=0.1;
-	if(key == 'e') zbola+=0.1;
-	if(key == 'f') xcam-=0.1;
-	if(key == 'r') xcam+=0.1;
-	if(key == 'g') ycam-=0.1;
-	if(key == 't') ycam+=0.1;
-	if(key == 'h') zcam-=0.1;
-	if(key == 'y') zcam+=0.1;*/
 	if(key == 'x') rot+=0.1;
 	if(key == 'c') rot-=0.1;
 	if(rot > 2*PI) rot -= 2*PI;
@@ -308,14 +243,12 @@ void funcaoMouse(int button, int state, int x, int y) {
 }
 
 int main(int argc, char **argv) {
-	//srand(time(NULL));
 	obstaculos[0] = make_pair(0, 90);
-	for(int i = 1; i < 50; i++){
+	for(int i = 1; i < QUANTIDADE_DE_OBSTACULOS; i++){
 		double a = rand()%360;
 		double b = a + 60 + rand()%(300-int(a));
 		if(a > b) swap(a, b);
 		obstaculos[i] = make_pair(a, b);
-		//obstaculos[i] = make_pair(90, 180);
 	}
 	
 	bola = Bola();
